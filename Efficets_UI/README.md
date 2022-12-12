615e13cc691561189e92b14baabc4b1658edc053
周肖 <zhoux@bimvr.net>
2022-12-12 14:37:50 +0800

    粒子效果

---
+++ Efficets_UI/README.md
@@ -0,0 +1,55 @@
+# 粒子效果
+
+维护者：周肖
+
+邮箱：1838079286@qq.com
+
+粒子效果的实现：使用Shader和unity的粒子系统(Particle System)
+
+
+
+## 效果展示
+
+### shader效果类
+
+流光效果
+
+![](EfficetsGif\图片反光效果.gif)
+
+
+
+消融效果
+
+![图片消融](EfficetsGif\图片消融.gif)
+
+
+
+###  粒子效果类（Particle System）
+
+全屏粒子发散效果
+
+![](EfficetsGif\全屏粒子效果.gif)
+
+
+
+小范围发散粒子效果
+
+![发散粒子](EfficetsGif\发散粒子.gif)
+
+
+
+绕圆形的粒子效果
+
+![绕圆形粒子](EfficetsGif\绕圆形粒子.gif)
+
+
+
+圆形拖尾粒子效果
+
+![圆形拖尾粒子](EfficetsGif\圆形拖尾粒子.gif)
+
+
+
+三种光晕粒子效果
+
+![三种粒子光晕](EfficetsGif\三种粒子光晕.gif)
---
+++ Efficets_UI/UIEfficets/Assets/ZSWEffiects/Scripts/UIParticleMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIParticleMove : MonoBehaviour
+{
+    [Header("运行后销毁路线对象")] public bool m_clearWhenStart;
+    public Transform m_allRoutePoints;
+
+    public List<Vector3> m_routePoints;
+    public int m_currentPointIndex;
+
+    [Header("粒子移动速度")] public float m_moveSpeed;
+
+    private void Start()
+    {
+        m_routePoints = new List<Vector3>();
+        for (int i = 0; i < m_allRoutePoints.childCount; i++)
+        {
+            m_routePoints.Add(m_allRoutePoints.GetChild(i).position);
+        }
+        transform.position = m_routePoints[0];
+
+        if (m_clearWhenStart)
+        {
+            for (int i = m_allRoutePoints.childCount - 1; i >= 0; i--)
+            {
+                Destroy(m_allRoutePoints.gameObject);
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(m_routePoints[m_currentPointIndex]);
+        transform.position += transform.forward * m_moveSpeed * Time.deltaTime; // = Vector3.LerpUnclamped(transform.position, m_routePoints[m_currentPointIndex], m_moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, m_routePoints[m_currentPointIndex]) <= 0.2f)
+        {
+            m_currentPointIndex = (m_currentPointIndex + 1) % m_routePoints.Count;
+        }
+    }
+}
